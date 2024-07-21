5b8f75dfbf46f992bad55dfe1fe774356ca65360
Ralf Schneider <12358@posteo.de>
2024-07-21 09:33:12 +0200

    feat: add author, description, and metadata to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='reqver',
     version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A tool to add version information to requirements.txt files',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/reqver',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -14,4 +20,10 @@ setup(
             'reqver=reqver.cli:main',
         ],
     },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
